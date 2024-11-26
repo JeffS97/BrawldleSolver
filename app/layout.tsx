ed854c088d0e4c8a40748465a0fcc93c736c3243
Jeffvinder Singh <40801372+JeffS97@users.noreply.github.com>
2024-11-26 20:21:08 +0800

    Added speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 // app/layout.tsx
 
 import './globals.css';  // Import global styles
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}<SpeedInsights /></body>
     </html>
   );
 }
